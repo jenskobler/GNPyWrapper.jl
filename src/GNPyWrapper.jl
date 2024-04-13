b8dfd732c599d3569c2bd22479b42107c75e7968
jenskobler <152269136+jenskobler@users.noreply.github.com>
2024-04-13 18:28:56 +0200

    Update GNPyWrapper.jl
    removed unnecessary code

--- src/GNPyWrapper.jl
+++ src/GNPyWrapper.jl
@@ -4,13 +4,6 @@ using PythonCall
 
 using DocStringExtensions
 
-export DummyStruct
-export dummymult10, dummyfunction
-
-export dummyfunction_02
-
-
-
 export second_layer_fiber_des
 
 export fiber_des
@@ -23,15 +16,16 @@ export roadm_des
 
 export fused_des
 
-#functions for creating
 export fiber_obj
-# test pythoncall stuff
+
 export fiber_obj_02
 
 export py_dir_funct
 
 export py_dir_funct_02
+
 export py_get_attr
+
 export py_set_attr
 
 export fiber_example_des
@@ -61,26 +55,8 @@ export fused_obj
 export create_full_real_path
 
 
-
-
-# export fiber_des,fiber_obj
-# export transceiver_des, transceiver_obj
-# export fused_des, fused_obj
-# export roadm_des, roadm_obj
-# export edfa_des, edfa_obj
-
 # Write your package code here.
-include("examplefile.jl")
 
-
-# #newly added
-# include("a_initialize_phase/create_description.jl")
-
-# include("a_initialize_phase/create_gnpy_object.jl")
-
-# include("a_initialize_phase/from_json_get_list_of_objects_in_path.jl")
-
-include("examplefile_02.jl")
 
 include("a_initialize_phase/examplefile_03.jl")
 include("a_initialize_phase/create_description.jl")
